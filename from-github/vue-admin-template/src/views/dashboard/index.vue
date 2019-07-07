0996c884ce025f96b9ef60fc919df134c671881d
hxb <295783719@qq.com>
2019-07-07 09:47:25 +0800

    test

---
+++ from-github/vue-admin-template/src/views/dashboard/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <el-button @click="method()">默认按钮</el-button>
+    <el-button type="text" @click="method()">打开嵌套表格的 Dialog</el-button>
+    <el-dialog title="收货地址" :visible.sync="dialogTableVisible">
+      <el-table :data="gridData">
+        <el-table-column property="date" label="日期" width="150"></el-table-column>
+        <el-table-column property="name" label="姓名" width="200"></el-table-column>
+        <el-table-column property="address" label="地址"></el-table-column>
+      </el-table>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  data() {
+    return {
+      dialogTableVisible: false
+    }
+  },
+  methods: {
+    method() {
+      this.dialogTableVisible = true
+      console.log("11111111111111")
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
